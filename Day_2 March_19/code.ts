355f007c92f602609aeed866f14d6cd608fea4b0
shrikrishna <shrikrishna.minksole@gmail.com>
2025-03-19 21:28:36 +0530

    Day-2

---
+++ Day_2 March_19/code.ts
@@ -0,0 +1,10 @@
+// TS is Strongly/Static typed
+let num = 10; // number datatype assigned
+// num = 20; // can update value
+num = "Hi"; // cant update datatype
+console.log(num);
+
+/*
+TS code execution flow:
+    TS Code ---> Compile ---> JS Code ---> output
+*/
---
+++ Day_2 March_19/first.js
@@ -0,0 +1,6 @@
+// JS is Dynamically typed
+let a = 10;
+// a = 20; // can update value
+a = "Hi"; // can update datatype
+
+console.log(a);

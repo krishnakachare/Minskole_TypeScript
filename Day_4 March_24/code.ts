2160d3df77d1bc91d7f47dbf9ecb7d622bcb3f9d
krishnakachare <shrikrishna.minksole@gmail.com>
2025-03-24 21:35:36 +0530

    Day-4

---
+++ Day_4 March_24/code.ts
@@ -0,0 +1,8 @@
+console.log("I am TS");
+// .ts --> compile --> .js --> output
+
+// JS + HTML
+// script:src
+
+// TS + HTML
+// .ts --> compile --> .js --> attach in HTML
---
+++ Day_4 March_24/types.ts
@@ -0,0 +1,16 @@
+// Datatypes in js: number, boolean, string, undefined, null, object, array, symbol
+
+// let Q = 30; // number
+// let Q = "Hi"; // Flexible
+
+// let Q: number = 30;
+let Q: number = "Hi"; // type restriction , Error
+// : number --> number is a type
+
+console.log(Q); // 30
+
+// Q = "Hi"; // string // Error
+// console.log(Q);
+
+// type : datatype hold by the variable as its value
+// How many types = Directly proportional to dataTypes present in javascript

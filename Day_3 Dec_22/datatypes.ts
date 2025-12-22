38374f549af086cc2f74f4481b799fa3b0bc6353
krishnakachare <shrikrishna.minksole@gmail.com>
2025-12-22 21:33:56 +0530

    Day_3

---
+++ Day_3 Dec_22/datatypes.ts
@@ -0,0 +1,12 @@
+/*
+# Datatypes in javaScript/TypeScript:
+    number, string, boolean, array, object, class, null, undefined, function, regex
+
+    type --> typescript
+    datatype --> javascript
+
+# Types in TypeScript:
+    1. Primitive(immutable): string, number, boolean, undefined, null, never, unknown, void
+    2. Special datatype: any
+    3. Non-Primitive(mutable, user defined): array, object, class, function, tuple, enum, interface
+ */

e1d129c774dabf79b0532c78eaf94e75a6ef13b1
krishnakachare <krishnakachare2@gmail.com>
2026-06-26 22:15:09 +0530

    Day-3

---
+++ Day_3 June_26/type.js
@@ -0,0 +1,17 @@
+"use strict";
+// 1. Type inferance:
+// infert --> Getting datatype
+// let a = 10;
+// console.log(a);
+// // strongly/static typed
+// a = "Hi"; // TS2322: Type 'string' is not assignable to type 'number'.
+// console.log(a);
+// 2. Type annotation:
+// Syntax --->   variableName: type = value
+// let a: number = 10;
+// console.log(a);
+// // strongly/static typed
+// a = "Hi"; // TS2322: Type 'string' is not assignable to type 'number'.
+// console.log(a);
+let b = "TS";
+console.log(b);
---
+++ Day_3 June_26/type.ts
@@ -0,0 +1,23 @@
+// 1. Type inferance:
+
+// infert --> Getting datatype
+
+// let a = 10;
+// console.log(a);
+
+// // strongly/static typed
+// a = "Hi"; // TS2322: Type 'string' is not assignable to type 'number'.
+// console.log(a);
+
+// 2. Type annotation:
+// Syntax --->   variableName: type = value
+
+// let a: number = 10;
+// console.log(a);
+
+// // strongly/static typed
+// a = "Hi"; // TS2322: Type 'string' is not assignable to type 'number'.
+// console.log(a);
+
+let b: string = "TS";
+console.log(b);
